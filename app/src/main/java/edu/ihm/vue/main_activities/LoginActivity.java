78e3b4d0053fcb5067d588ba0bec991c36508aad
Logan Lucas <logan.lucas83@gmail.com>
2024-05-24 12:38:19 +0200

    login HTTP request

--- app/src/main/java/edu/ihm/vue/main_activities/LoginActivity.java
+++ app/src/main/java/edu/ihm/vue/main_activities/LoginActivity.java
@@ -1,17 +1,25 @@
 package edu.ihm.vue.main_activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import edu.ihm.vue.R;
 import edu.ihm.vue.models.User;
+import edu.ihm.vue.models.web_service_models.Credentials;
+import edu.ihm.vue.models.web_service_models.Token;
+import edu.ihm.vue.web_service.WebService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
     Button connexion;
@@ -22,28 +30,45 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         connexion = findViewById(R.id.connecter);
-        connexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView username = (TextView) findViewById(R.id.email);
-                TextView password = (TextView) findViewById(R.id.password);
-                connect(username.getText().toString(), password.getText().toString());
-            }
+        connexion.setOnClickListener(v -> {
+            TextView username = findViewById(R.id.email);
+            TextView password = findViewById(R.id.password);
+            connect(username.getText().toString(), password.getText().toString());
         });
     }
 
     private void connect(String username, String password) {
+        WebService.getInstance().getService().login(new Credentials(username, password)).enqueue(new Callback<Token>() {
+            @Override
+            public void onResponse(@NonNull Call<Token> call, @NonNull Response<Token> response) {
+                if (response.isSuccessful()) {
+                    assert response.body() != null;
+                    System.out.println(response.body().getToken());
+                } else {
+                    Toast.makeText(LoginActivity.this, "Nom de compte ou mot de passe incorrect!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Token> call, @NonNull Throwable throwable) {
+                Toast.makeText(LoginActivity.this, "Failed !", Toast.LENGTH_LONG).show();
+            }
+        });
+
         if (username.equals("user") && password.equals("user")) {
             Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-            intent.putExtra("user",(Parcelable) new User("1","Nougaret","Adrien", User.role.PARTICULIER));
+            intent.putExtra("user",(Parcelable) new User("1","Nougaret","Adrien", User.Role.PARTICULIER));
             startActivity(intent);
         } else if (username.equals("admin") && password.equals("admin")) {
             Intent intent = new Intent(getApplicationContext(),AgentActivity.class);
-            intent.putExtra("user",(Parcelable) new User("2","Hauchart","Lucas", User.role.FONCTIONNAIRE));
+            intent.putExtra("user",(Parcelable) new User("2","Hauchart","Lucas", User.Role.FONCTIONNAIRE));
             startActivity(intent);
         } else {
             Toast.makeText(LoginActivity.this, "Nom de compte ou mot de passe incorrect!", Toast.LENGTH_SHORT).show();
         }
     }
 
+    private void getMe(String token) {
+
+    }
 }
--- app/src/main/java/edu/ihm/vue/web_service/GreenTrackAPI.java
+++ app/src/main/java/edu/ihm/vue/web_service/GreenTrackAPI.java
@@ -1,6 +1,9 @@
 package edu.ihm.vue.web_service;
 
 import edu.ihm.vue.models.Signalement;
+import edu.ihm.vue.models.User;
+import edu.ihm.vue.models.web_service_models.Credentials;
+import edu.ihm.vue.models.web_service_models.Token;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
@@ -11,4 +14,8 @@ public interface GreenTrackAPI {
     @Headers("Content-Type: application/json")
     @POST("reports")
     Call<Signalement> createReport(@Body Signalement report, @Header("Authorization") String bearer);
+
+    @Headers("Content-Type: application/json")
+    @POST("auth/login")
+    Call<Token> login(@Body Credentials credentials);
 }
